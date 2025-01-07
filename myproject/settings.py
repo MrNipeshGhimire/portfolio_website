8d36823cd7f02297f7ced9d2d5613326baf225fb
MrNipeshGhimire <nipeshincbca2021@gmail.com>
2025-01-07 12:53:27 +0545

    django

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tuxs62mia*2pkp#8rw19pfld9+233rjz5iwt)4x-bbh&^$z$l('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['nipeshghimire.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
