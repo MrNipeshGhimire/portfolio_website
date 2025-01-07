dcf604a8888934c576680b8dffc5d3b754f75977
MrNipeshGhimire <nipeshincbca2021@gmail.com>
2025-01-07 13:32:15 +0545

    commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views.main_view import index, contactForm
 
 urlpatterns = [
-    path('',index, name="index"),
+    path('/',index, name="index"),
     path('contact/',contactForm, name="contact" )
     
 ]
